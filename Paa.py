1c8f8d5090b88cd7f2c267a287e17b5360a483a7
Caio Rodrigues <88396592+CaioRod41@users.noreply.github.com>
2024-09-10 02:03:08 -0300

    Update Paa.py

--- Paa.py
+++ Paa.py
@@ -6,16 +6,6 @@ def linear_search(arr, target):
             return i
     return -1
 
-#Ordenação por seleção
-
-def selection_sort(arr):
-    for i in range(len(arr)):
-        min_idx = i
-        for j in range(i+1, len(arr)):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
 
 #Busca binária
 def binary_search(arr, target):
